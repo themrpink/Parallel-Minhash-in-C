fbe05ba5bf0067fb653ed777190c94d661c881df
Stefano <stefanourani@yahoo.it>
2020-12-23 02:04:10 +0100

    modificata la funzione di hash in omp

--- omp/hash_FNV_1.c
+++ omp/hash_FNV_1.c
@@ -1,6 +1,30 @@
 #include "hash_FNV_1.h"
 
-int hash_FNV_1(char *shingle, int const prime_i, unsigned long long *hash){
+int hash_FNV_1(char *shingle, unsigned long long *hash){
+    
+    unsigned long long FNV_offset_basis = 0xcbf29ce484222325LL;
+    unsigned long long prime = 1099511628211;
+
+    *hash = FNV_offset_basis;
+    //s#pragma omp parallel for num_threads(4)
+    for(int i=0; i<K_SHINGLE; i++){
+        (*hash) *= prime;
+        (*hash) ^= shingle[i];
+    }
+    return 0;
+
+}
+
+
+void hash_fun(unsigned long long hashed_shingle, unsigned long long *hash, int i){
+
+     *hash = hashed_shingle ^ rands[i];
+}
+
+
+
+
+int hash_FNV_200(char *shingle, int const prime_i, unsigned long long *hash){
     
     unsigned long long FNV_offset_basis = 0xcbf29ce484222325LL;
 
@@ -10,7 +34,7 @@ int hash_FNV_1(char *shingle, int const prime_i, unsigned long long *hash){
     }
 
     *hash = FNV_offset_basis;
-    //s#pragma omp parallel for num_threads(4)
+
     for(int i=0; i<K_SHINGLE; i++){
         (*hash) *= primes[prime_i];
         (*hash) ^= shingle[i];
--- omp/test_main_omp.c
+++ omp/test_main_omp.c
@@ -2,7 +2,7 @@
 #include <string.h>
 #include <stdlib.h>
 #include <omp.h>
-
+#include <stdint.h>
 #include "hash_FNV_1.c"
 #include "shingle_extract_omp.c"
 
@@ -10,12 +10,18 @@
 #define K_SHINGLE 54
 #define MAX_LONG_LONG 0xcbf29ce484222325LL
 
+/*
+COMPILE OMP: gcc -g -Wall -fopenmp -o  FILE FILE.C -lm
+
+*/
 
 
 int main(int argc, char *argv[]){
 
     unsigned long long hash;
     unsigned long long minhash; 
+    unsigned long long *signatures =  (unsigned long long *)malloc(200*sizeof(unsigned long long *));
+    unsigned long long * hashed_shingles; 
     long size;
     long tot_shingles;
     char **shingles;
@@ -36,7 +42,7 @@ int main(int argc, char *argv[]){
     fseek(fp, 0, SEEK_SET); // seek back to beginning of file
 
     tot_shingles = size - K_SHINGLE + 1;
-
+    
     //alloca lo spazio per gli shingles
     shingles = malloc(tot_shingles * (sizeof(char*)));
 
@@ -56,60 +62,67 @@ int main(int argc, char *argv[]){
     end = omp_get_wtime();
     printf("\n \n opm version parallel for \n f: shingle_extract_buf_r \n number of threads: %d\n time: %f\n",thread_count, end - start);
 
-    fseek(fp, 0, SEEK_SET);
-    start = omp_get_wtime();
-    shingle_extract_buf(fp, size, thread_count, shingles);
-    end = omp_get_wtime();
-    printf("\n\n serial version simple loop\n f: shingle_extract_buf \n number of threads: %d\n time: %f\n",thread_count,  end - start);
 
 
 /*
     tempi di esecuzione del minhash seriale
 */
-
+    fseek(fp, 0, SEEK_SET);
     start = omp_get_wtime();
+    shingle_extract_buf(fp, size, thread_count, shingles);
+    end = omp_get_wtime();
+    printf("\n\n serial version simple loop\n f: shingle_extract_buf \n number of threads: %d\n time: %f\n", 1 ,  end - start);
+
+
+
+    hashed_shingles = (unsigned long long *)malloc(tot_shingles*sizeof(unsigned long long *));
+    start = omp_get_wtime();
+
+    for(int j=0; j<tot_shingles; j++){
+        //lancia la prima funzione di hash su ogni shingle 
+        hash_FNV_1(shingles[j], &hash);
+        hashed_shingles[j] = hash;
+
+        if(hash<minhash)
+            minhash=hash;
+    }
+
+    signatures[0]=minhash;
+
     //applica ogni funzione di hash ogni volta su tutti gli shingle, ricava i minhash
-    for(int i=0; i<PRIMES_SIZE; i++){
+   // #pragma omp parallel for num_threads(2)
+    for(int i=1; i<PRIMES_SIZE; i++){
 
         minhash = MAX_LONG_LONG;
-       // #pragma omp parallel for num_threads(thread_count)
+      
         for(int j=0; j<tot_shingles; j++){
             //lancia la funzione di hash su ogni shingle 
-            hash_FNV_1(shingles[j], i, &hash);
-
+            hash_fun(hashed_shingles[j], &hash, i);
             if(hash<minhash)
                 minhash=hash;
-        }
-        
-       // printf("%llu \n", minhash);
+        }       
+        signatures[i]=minhash;
     }
     
     end = omp_get_wtime();
-    printf("\n\n serial version hash\n f: hash \n number of threads: %d\n time: %f\n",thread_count,  end - start);
+    printf("\n\n serial version hash\n f: hash \n number of threads: %d\n time: %f\n", 1 ,  end - start);
 
-
-
-    /*
-        primo tentativo non efficiente di parallelizzazione del minhash
-    */
 
     start = omp_get_wtime();
     //#pragma omp parallel for num_threads(thread_count)
     for(int i=0; i<PRIMES_SIZE; i++){
 
         minhash = MAX_LONG_LONG;
-            #pragma omp parallel for num_threads(thread_count)
             
-                for(int j=0; j<tot_shingles; j++){
-                    //lancia la funzione di hash su ogni shingle 
-                    hash_FNV_1(shingles[j], i, &hash);
+        for(int j=0; j<tot_shingles; j++){
+            //lancia la funzione di hash su ogni shingle 
+            hash_FNV_200(shingles[j], i, &hash);
 
-                    if(hash<minhash)
-                        minhash=hash;
-                }
-            
-  //      printf("%llu \n", minhash);
+            if(hash<minhash)
+                minhash=hash;
+        }
     }
+    
     end = omp_get_wtime();
     printf("\n\n parallel version hash omp\n f: hash \n number of threads: %d\n time: %f\n",thread_count,  end - start);
 
