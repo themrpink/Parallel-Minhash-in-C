85aeb7432bdd59a7b8e2ce63c85297be498841fe
Stefano <stefanourani@yahoo.it>
2020-12-24 01:42:37 +0100

    qualche parallelizzazione

--- serial/shingle_extract.h
+++ serial/shingle_extract.h
@@ -3,5 +3,7 @@
 
 
 int shingle_extract_buf(FILE * fp, const long filesize, char **shingles);
+
 void print_shingles(int tot_shingles, char **shingles);
+
 char** get_shingles(FILE *fp, long *tot_shingles);
