56d1c8f17775e68f1ab11f6431945f64bdb7fef0
dani2612 <dani2612@hotmail.it>
2020-12-25 20:23:06 +0100

    Prima implementazione tokenizer.c

--- serial/tokenizer.c
+++ serial/tokenizer.c
@@ -1,5 +1,45 @@
-//
-// Created by dani2612 on 19/12/20.
-//
 
 #include "tokenizer.h"
+#include "stdio.h"
+#include "ctype.h"
+
+#define EMPTY ""
+
+char* get_file_string_cleaned(const char* file_path,long* fileLength){
+    char * buffer = 0;
+    FILE * fp = fopen (file_path, "rb");
+    if (fp){
+        fseek (fp, 0, SEEK_END);
+        fileLength = ftell (fp);
+        fseek (fp, 0, SEEK_SET);
+        buffer = malloc (length);
+        if (buffer)
+        {
+            fread (buffer, 1, fileLength, fp);
+        }
+        fclose (f);
+    }
+    if (buffer){
+       compress_spaces(testo);
+       testo=tolower(testo);
+       return testo;
+    }else{
+        fileLength=0;
+        return EMPTY;
+    }
+}
+
+void compress_spaces(char *str){
+    char *dst = str;
+    for (; *str; ++str) {
+        *dst++ = *str;
+
+        if (isspace(*str)) {
+            do ++str;
+            while (isspace(*str));
+
+            --str;
+        }
+    }
+    *dst = 0;
+}
--- serial/tokenizer.h
+++ serial/tokenizer.h
@@ -2,7 +2,5 @@
 // Created by dani2612 on 19/12/20.
 //
 
-#ifndef MINHASHPROJECT_TOKENIZER_H
-#define MINHASHPROJECT_TOKENIZER_H
-
-#endif //MINHASHPROJECT_TOKENIZER_H
+char* get_file_string_cleaned(const char* file_path,long* fileLength)
+void compress_spaces(char *str);
