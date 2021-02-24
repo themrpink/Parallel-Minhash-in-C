50d57bfe602adad1495a4334f07e622bea287116
Stefano <stefanourani@yahoo.it>
2021-02-24 16:53:39 +0100

    aggiunto minhash.c

--- pthread_prod_cons/min_hash.h
+++
@@ -1,7 +0,0 @@
-
-
-#include "prod_cons.h"
-
-void *minHash(void * args);
-
-struct getSignatures_producer_args* init_getSignatures_args(long numb_shingles, char **shingles, long long unsigned **minhashDocumenti);
